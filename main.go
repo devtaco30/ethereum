826815dbd43e60c0b7d636fdce20ef654a8d4f93
devtaco30 <devtaco30@gmail.com>
2024-02-24 10:13:33 +0900

    lec-13-pointer

--- main.go
+++ main.go
@@ -18,4 +18,15 @@ func main() {
 	data, _ := json.Marshal(v)
 	fmt.Println(string(data))
 
+	// 포인터로 값 변경
+	num := 1
+	fmt.Println("before num: %d", num)
+	numAddress := &num
+	*numAddress = 2
+	fmt.Println("after num: %d", num)
+
+	var numPointer *int = &num // int 의 포인터를 만들어서 num 의 주소를 할당
+	fmt.Println("num address: ", &num)
+	fmt.Println("num value by reference : ", *numPointer)
+
 }
